e8e4ed6001c09bf8af6b2aa40f94a791522b8bb9
wormzoop <446706489@qq.com>
2017-07-19 11:06:11 +0800

    commit

---
+++ struts-mybatis/src/main/java/com/zoop/action/UserAction.java
@@ -0,0 +1,13 @@
+package com.zoop.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport{
+
+	private static final long serialVersionUID = -7262966706665445470L;
+	
+	public String execute() throws Exception{
+		return SUCCESS;
+	}
+
+}
